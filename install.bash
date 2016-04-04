756ec37cd7bccbbbc67c6954d59bf1937108f608
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2016-04-04 13:59:48 -0700

    Filled out and tested install scripts.
    There are a few features left to add (such as automatically cloning and
    installing zprezto).

--- install.bash
+++ install.bash
@@ -1,12 +1,46 @@
 #!/bin/bash -e
 
+# Keep track of starting directory.
+CWD=`pwd`
+
 # Install Base16 Shell.
-# TODO: Check if this exists, if it does, the update it.
-git clone https://github.com/chriskempson/base16-shell.git \
-    ~/.config/base16-shell
+dest_dir="$HOME/.config/base16-shell"
+if [ -d "$dest_dir" ]; then
+    cd $dest_dir
+    git pull
+    cd $CWD
+else
+    git clone https://github.com/chriskempson/base16-shell.git $dest_dir
+fi
 
-# TODO: Build python3 vim.
+# Prompt user to ask if they want to build vim with python3 support (this takes
+# ~5 minutes).
+read -p "Do you want to install/update vim with Python3 support? [y/n] " yn
+if [ "$yn" == "y" ]; then
+    vim_src_dir="vim_src"
+    git clone https://github.com/vim/vim.git $vim_src_dir
+    cd $vim_src_dir
+    python3_config_dir=`python3-config --configdir`
+    ./configure \
+        --enable-perlinterp=yes \
+        --enable-python3interp=yes \
+        --enable-rubyinterp=yes \
+        --enable-cscope \
+        --enable-gui=auto \
+        --enable-gnome-check=yes \
+        --with-features=huge \
+        --enable-multibyte \
+        --with-x \
+        --with-python3-config-dir=$python3_config_dir
+    make
+    sudo make install
+    cd ..
 
-# TODO: Run vim install.
+    # Clean up.
+    rm -rf $vim_src_dir
+fi
+
+# Customize vim installation.
+./vim/install_vim.bash
 
 # TODO: Clone my zprezto repo and install that.
--- vim/install_vim.bash
+++ vim/install_vim.bash
@@ -1,45 +1,75 @@
 #!/bin/bash -e
 
+# If a local git repo exists, update it. If it doesn't exist, clone a new repo
+# in the expected location.
+#
+# Inputs:
+#   $1 : path to remote git repository to clone/pull.
+#   $2 : path to local git repository
+git_clone_or_update(){
+    if [ -d "$2" ]; then
+        echo "Updating $2 from $1"
+        cd $2
+        git pull
+        cd ~
+    else
+        echo "Cloning $1 into $2"
+        git clone $1 $2
+    fi
+}
+
 # Install vimrc.
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+VIMRC="$HOME/.vimrc"
+if [ ! -e "$VIMRC" ]; then
+    ln -s ~/dotfiles/vim/vimrc $VIMRC
+fi
+
+# Install flake8 configuration for vim-flake8.
+FLAKE8_CONFIG="$HOME/.config/flake8"
+if [ ! -e "$FLAKE8_CONFIG" ]; then
+    ln -s ~/dotfiles/vim/flake8 $FLAKE8_CONFIG
+fi
 
 # Create necessary directories.
-# TODO: Turn these into variables, use them later in the file.
-mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/indent
+VIM_AUTOLOAD="$HOME/.vim/autoload"
+VIM_BUNDLE="$HOME/.vim/bundle"
+mkdir -p $VIM_AUTOLOAD $VIM_BUNDLE
 
 # Install pathogen. Taken from:
 #   https://github.com/tpope/vim-pathogen
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso "$VIM_AUTOLOAD/pathogen.vim" https://tpo.pe/pathogen.vim
 
 # Lots of customizations taken from:
 #   https://realpython.com/blog/python/vim-and-python-a-match-made-in-heaven/
 
-# TODO: Check if this exists, if it does, the update it.
-git clone https://github.com/tmhedberg/SimpylFold.git ~/.vim/bundle/SimpylFold
+# Smart code folding in Python files.
+git_clone_or_update https://github.com/tmhedberg/SimpylFold.git \
+    "$VIM_BUNDLE/SimpylFold"
 
-# TODO: Check if this exists, if it does, the update it.
-git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+# Syntax checking.
+git_clone_or_update https://github.com/scrooloose/syntastic.git \
+    "$VIM_BUNDLE/syntastic"
 
-# TODO: Check if this exists, if it does, the update it.
-git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
+# PEP8 checking in Python files.
+git_clone_or_update https://github.com/nvie/vim-flake8.git \
+    "$VIM_BUNDLE/vim-flake8"
 
-# TODO: Check if this exists, if it does, the update it.
-git clone https://github.com/vim-scripts/indentpython.vim.git \
-    ~/.vim/bundle/indentpython.vim
+# Make sure Python indenting conforms to PEP8.
+git_clone_or_update https://github.com/vim-scripts/indentpython.vim.git \
+    "$VIM_BUNDLE/indentpython.vim"
 
-# TODO: Check if this exists, if it does, the update it.
-git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+# Powerful searching from within vim.
+git_clone_or_update https://github.com/ctrlpvim/ctrlp.vim.git \
+    "$VIM_BUNDLE/ctrlp.vim"
 
-# TODO: Check if this exists, if it does, the update it.
-git clone https://github.com/vim-airline/vim-airline.git \
-    ~/.vim/bundle/vim-airline
+# Awesome status bar.
+git_clone_or_update https://github.com/vim-airline/vim-airline.git \
+    "$VIM_BUNDLE/vim-airline"
 
-# TODO: Check if this exists, if it does, the update it.
-git clone https://github.com/vim-scripts/Align.git ~/.vim/bundle/Align
+# Column-align multiple lines.
+git_clone_or_update https://github.com/vim-scripts/Align.git \
+    "$VIM_BUNDLE/Align"
 
-# TODO: Check if this exists, if it does, the update it.
-git clone https://github.com/chriskempson/base16-vim.git \
-    ~/.vim/bundle/base16-vim
-
-# Install flake8 configuration for vim-flake8.
-ln -s ~/dotfiles/vim/flake8 ~/.config/flake8
+# Base16 colors in vim.
+git_clone_or_update https://github.com/chriskempson/base16-vim.git \
+    "$VIM_BUNDLE/base16-vim"
