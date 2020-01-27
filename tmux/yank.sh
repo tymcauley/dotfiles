499f9dfcb033f8ad1c03906bb63d358c8874d08a
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2020-01-27 14:57:51 +0200

    Added more updates to tmux copy script.
    Updates from:
        https://medium.com/free-code-camp/tmux-in-practice-integration-with-system-clipboard-bcd72c62ff7b

--- tmux/yank.sh
+++ tmux/yank.sh
@@ -1,10 +1,37 @@
 #!/bin/bash -e
 
-# get data either form stdin or from file
+is_app_installed() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Get data either from stdin or from file.
 buf=$(cat "$@")
 
+# Resolve copy backend: pbcopy (macOS), reattach-to-user-namespace (macOS), xclip/xsel (Linux).
+# Only use Linux `xsel`/`xclip` backends if the session isn't over SSH.
+copy_backend=""
+if is_app_installed pbcopy; then
+    copy_backend="pbcopy"
+elif is_app_installed reattach-to-user-namespace; then
+    copy_backend="reattach-to-user-namespace pbcopy"
+elif [ -n "${DISPLAY-}" ] && is_app_installed xsel; then
+    copy_backend="xsel -i --clipboard"
+elif [ -n "${DISPLAY-}" ] && is_app_installed xclip; then
+    copy_backend="xclip -i -f -selection primary | xclip -i -selection clipboard"
+fi
+
+# If copy backend is resolved, copy and exit.
+if [ -n "$copy_backend" ]; then
+    printf "%s" "$buf" | eval "$copy_backend"
+    exit
+fi
+
+# If none of the copy backends resolved, then fall back to using OSC 52 escape
+# sequences. In theory, these should work very well, and transmit across remote
+# connections. In practice, not many shells support them.
+
 # Copy via OSC 52 ANSI escape sequence to controlling terminal
-buflen=$( printf %s "$buf" | wc -c )
+buflen=$(printf %s "$buf" | wc -c)
 
 # https://sunaku.github.io/tmux-yank-osc52.html
 # The maximum length of an OSC 52 escape sequence is 100_000 bytes, of which
@@ -14,11 +41,11 @@ maxlen=74994
 
 # warn if exceeds maxlen
 if [ "$buflen" -gt "$maxlen" ]; then
-    printf "input is %d bytes too long" "$(( buflen - maxlen ))" >&2
+    printf "input is %d bytes too long" "$((buflen - maxlen))" >&2
 fi
 
 # build up OSC 52 ANSI escape sequence
-esc="\033]52;c;$( printf %s "$buf" | head -c $maxlen | base64 | tr -d '\r\n' )\a"
+esc="\033]52;c;$(printf %s "$buf" | head -c $maxlen | base64 | tr -d '\r\n')\a"
 esc="\033Ptmux;\033$esc\033\\"
 
 # resolve target terminal to send escape sequence
@@ -27,4 +54,4 @@ esc="\033Ptmux;\033$esc\033\\"
 pane_active_tty=$(tmux list-panes -F "#{pane_active} #{pane_tty}" | awk '$1=="1" { print $2 }')
 target_tty="${SSH_TTY:-$pane_active_tty}"
 
-printf "$esc" > "$target_tty"
+printf "$esc" >"$target_tty"
