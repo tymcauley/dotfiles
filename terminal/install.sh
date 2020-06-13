11ca0a8a82f97b713c626f9d0c690a03a251b378
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2020-06-12 17:28:02 -0700

    Add kitty (terminal) configuration

--- terminal/install.sh
+++ terminal/install.sh
@@ -2,6 +2,7 @@
 
 # Customize terminal configuration.
 #   Alacritty: https://github.com/jwilm/alacritty
+#   Kitty: https://sw.kovidgoyal.net/kitty/index.html
 
 cd $(dirname $0)
 
@@ -15,5 +16,8 @@ source $DOTFILES_LIB
 
 # Install alacritty config file.
 install_file "$HOME/.config/alacritty/alacritty.yml" "$(pwd)/alacritty.yml"
+
+# Install kitty config file.
+install_file "$HOME/.config/kitty/kitty.conf" "$(pwd)/kitty.conf"
 
 cd - > /dev/null
