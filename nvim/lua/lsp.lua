2f060d9796d1489e85b4a445c45e4848ac7474e4
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2021-06-28 18:27:23 -0700

    nvim: Move 'rust-tools' setup to lsp.lua

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -40,3 +40,51 @@ vim.cmd [[augroup lsp]]
 vim.cmd [[autocmd!]]
 vim.cmd [[autocmd FileType scala,sbt lua require("metals").initialize_or_attach(metals_config)]]
 vim.cmd [[augroup end]]
+
+-- rust-tools (simrat39/rust-tools.nvim)
+
+require'rust-tools'.setup {
+    tools = { -- rust-tools options
+        inlay_hints = {
+            -- prefix for parameter hints
+            parameter_hints_prefix = "<- ",
+
+            -- prefix for all the other hints (type, chaining)
+            other_hints_prefix  = "=> ",
+
+            -- whether to align to the length of the longest line in the file
+            max_len_align = false,
+
+            -- whether to align to the extreme right or not
+            right_align = false,
+        },
+
+        hover_actions = {
+            -- the border that is used for the hover window
+            -- see vim.api.nvim_open_win()
+            border = {
+              {"╭", "FloatBorder"},
+              {"─", "FloatBorder"},
+              {"╮", "FloatBorder"},
+              {"│", "FloatBorder"},
+              {"╯", "FloatBorder"},
+              {"─", "FloatBorder"},
+              {"╰", "FloatBorder"},
+              {"│", "FloatBorder"}
+            },
+        }
+    },
+
+    -- all the opts to send to nvim-lspconfig
+    -- these override the defaults set by rust-tools.nvim
+    -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+    server = {
+        settings = {
+            ["rust-analyzer"] = {
+                checkOnSave = {
+                    command = "clippy"
+                }
+            }
+        }
+    }, -- rust-analyer options
+}
