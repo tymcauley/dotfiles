e84786460778988680beddae27bbc90da8263066
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2021-07-22 15:49:53 -0700

    nvim: Disable more built-in plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -280,9 +280,30 @@ opt('w', 'foldlevel', 99)
 -- Set pop-up-menu transparency
 opt('o', 'pumblend', 30)
 
--- Disable netrw (see ':help netrw-noload')
-g.loaded_netrw       = 1
-g.loaded_netrwPlugin = 1
+-- Disable unnecessary built-in plugins
+local disabled_built_ins = {
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "gzip",
+    "zip",
+    "zipPlugin",
+    "tar",
+    "tarPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "rrhelper",
+    "spellfile_plugin",
+    "matchit"
+}
+for _, plugin in pairs(disabled_built_ins) do
+    g["loaded_" .. plugin] = 1
+end
 
 -- LSP settings
 
