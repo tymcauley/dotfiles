f4c2dc4fe76e931b2348f7369913a5beff2aa3e2
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2021-08-23 11:13:18 -0700

    nvim: Remove popup.nvim dependency from telescope
    See here for more info:
        https://github.com/nvim-telescope/telescope.nvim/pull/1102
        https://github.com/nvim-telescope/telescope.nvim/pull/1103

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -70,7 +70,6 @@ return require('packer').startup({function()
     use {
         'nvim-telescope/telescope.nvim',
         requires = {
-            {'nvim-lua/popup.nvim'},
             {'nvim-lua/plenary.nvim'},
             {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'},
         }
