b37c28839071444ae97f587f3449e50afd457a2f
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2021-09-28 15:32:54 -0700

    nvim: Remove vim-fugitive plugin
    gitsigns.nvim has replaced it.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -33,7 +33,6 @@ return require('packer').startup({function()
     use {'tpope/vim-repeat'}
 
     -- Add nice integration with git
-    use {'tpope/vim-fugitive'}
     use {
         'lewis6991/gitsigns.nvim',
         requires = {'nvim-lua/plenary.nvim'}
