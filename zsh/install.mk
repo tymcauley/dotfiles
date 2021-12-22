32c697fec95860550c8ae448d8db0e11433f2dbc
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2021-12-22 07:18:45 -0800

    zsh: Replace zsh-vi-mode plugin with vi-mode
    The vi-mode plugin is built-in to oh-my-zsh, and seems a bit more
    stable.

--- zsh/install.mk
+++ zsh/install.mk
@@ -17,4 +17,3 @@ zsh:
 	./install_file.sh zsh/editor.zsh     ~/.config/editor.zsh
 	./install_file.sh zsh/fzf-config.zsh ~/.config/fzf-config.zsh
 	./git_clone_or_pull.sh https://github.com/romkatv/powerlevel10k.git $(OMZ_DIR)/custom/themes/powerlevel10k
-	./git_clone_or_pull.sh https://github.com/jeffreytse/zsh-vi-mode    $(OMZ_DIR)/custom/plugins/zsh-vi-mode
