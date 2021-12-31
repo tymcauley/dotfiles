fe9d2cc24fffd3f761e61720f6fa7b1335fee7d5
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2021-12-31 12:43:43 -0800

    tmux: Change colorscheme to tokyonight
    Downloaded from here:
    
    https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/kitty_tokyonight_storm.conf

--- tmux/install.mk
+++ tmux/install.mk
@@ -2,7 +2,7 @@
 ALL_TARGETS += tmux
 
 tmux:
-	./install_file.sh tmux/tmux.conf   ~/.tmux.conf
-	./install_file.sh tmux/status.conf ~/.tmux/status.conf
-	./install_file.sh tmux/yank.sh     ~/.tmux/yank.sh
+	./install_file.sh tmux/tmux.conf                  ~/.tmux.conf
+	./install_file.sh tmux/tmux_tokyonight_storm.tmux ~/.tmux/tmux_tokyonight_storm.tmux
+	./install_file.sh tmux/yank.sh                    ~/.tmux/yank.sh
 	./git_clone_or_pull.sh https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
