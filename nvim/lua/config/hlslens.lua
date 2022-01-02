bbc79f57254f39cf1d4c0b5e748011fd5d2d6e07
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2022-01-01 17:52:37 -0800

    nvim: Don't center each search result
    This caused the window to jump around a bit too much.

--- nvim/lua/config/hlslens.lua
+++ nvim/lua/config/hlslens.lua
@@ -7,11 +7,9 @@ require("hlslens").setup({
 
 local start_hlslens = "<Cmd>lua require('hlslens').start()<CR>"
 
--- The 'zzzv' sequence means the following:
---  zz -> center the screen on search result
---  zv -> open any code folds so we can see the search result
-utils.map("n", "n", "<Cmd>execute('normal! ' . v:count1 . 'nzzzv')<CR>" .. start_hlslens, { silent = true })
-utils.map("n", "N", "<Cmd>execute('normal! ' . v:count1 . 'Nzzzv')<CR>" .. start_hlslens, { silent = true })
+-- The 'zv' command opens any code folds so we can see the search result
+utils.map("n", "n", "<Cmd>execute('normal! ' . v:count1 . 'nzv')<CR>" .. start_hlslens, { silent = true })
+utils.map("n", "N", "<Cmd>execute('normal! ' . v:count1 . 'Nzv')<CR>" .. start_hlslens, { silent = true })
 utils.map("n", "*", "*" .. start_hlslens)
 utils.map("n", "#", "#" .. start_hlslens)
 utils.map("n", "g*", "g*" .. start_hlslens)
