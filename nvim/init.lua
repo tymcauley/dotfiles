f414c0aea0ee57e9db62d4263e01e93b971639c8
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2022-04-02 15:01:52 -0700

    nvim: Use `vim.opt` to set options
    Also remove shortcuts, they don't save a lot of space, and they make
    things a bit more confusing.

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,27 +2,15 @@
 
 local utils = require("utils")
 
-local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
-local fn = vim.fn -- to call Vim functions e.g. fn.bufnr()
-local g = vim.g -- a table to access global variables
-
-local scopes = { o = vim.o, b = vim.bo, w = vim.wo }
-
-local function opt(scope, key, value)
-    scopes[scope][key] = value
-    if scope ~= "o" then
-        scopes["o"][key] = value
-    end
-end
 
 --
 -- Plugins
 --
 
 -- Bootstrap packer installation
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    vim.fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
     vim.api.nvim_command("packadd packer.nvim")
 end
 
@@ -39,8 +27,8 @@ vim.api.nvim_create_autocmd(
 -- Mappings
 --
 
-g.mapleader = [[,]] -- map by , instead of /
-g.maplocalleader = [[\]] -- map local by \
+vim.g.mapleader = [[,]] -- map by , instead of /
+vim.g.maplocalleader = [[\]] -- map local by \
 
 -- clear search
 vim.keymap.set("n", "<leader>/", ":nohlsearch<CR>", { silent = true })
@@ -51,9 +39,9 @@ vim.keymap.set("n", "<leader>s", "<Cmd>set spell!<CR>", { silent = true })
 -- Delete trailing whitespace
 vim.keymap.set("n", "<leader>w", function()
     if not vim.o.binary and vim.o.filetype ~= "diff" then
-        local current_view = fn.winsaveview()
-        cmd([[keeppatterns %s/\s\+$//e]])
-        fn.winrestview(current_view)
+        local current_view = vim.fn.winsaveview()
+        vim.cmd([[keeppatterns %s/\s\+$//e]])
+        vim.fn.winrestview(current_view)
     end
 end, { silent = true })
 
@@ -78,60 +66,60 @@ vim.keymap.set("n", "<space>", "za")
 --
 
 -- Set the command window height to 2 lines, to avoid many cases of having to 'press <Enter> to continue'
-opt("o", "cmdheight", 2)
+vim.opt.cmdheight = 2
 
 -- Don't show what mode we're in on the last line, the status line takes care of that
-opt("o", "showmode", false)
+vim.opt.showmode = false
 
 -- Enable global statusline
-opt("o", "laststatus", 3)
+vim.opt.laststatus = 3
 
 -- This allows you to switch from an unsaved buffer without saving it first. Also allows you to keep an undo history
 -- for multiple files. Vim will complain if you try to quit without saving, and swap files will keep you safe if your
 -- computer crashes.
-opt("o", "hidden", true)
+vim.opt.hidden = true
 
 -- Search settings
-opt("o", "hlsearch", true) -- highlight search
-opt("o", "incsearch", true) -- reveal search incrementally as typed
-opt("o", "ignorecase", true) -- case-insensitive match...
-opt("o", "smartcase", true) -- ...except when uppercase letters are given
+vim.opt.hlsearch = true -- highlight search
+vim.opt.incsearch = true -- reveal search incrementally as typed
+vim.opt.ignorecase = true -- case-insensitive match...
+vim.opt.smartcase = true -- ...except when uppercase letters are given
 
 -- Instead of failing a command because of unsaved changes, raise a prompt asking if you wish to save changed files
-opt("o", "confirm", true)
+vim.opt.confirm = true
 
 -- Use visual bell instead of beeping when doing something wrong
-opt("o", "visualbell", true)
-opt("o", "errorbells", false)
+vim.opt.visualbell = true
+vim.opt.errorbells = false
 
 -- Show special characters
-opt("w", "list", true)
-local listchars = ""
--- Place a '#' in the last column when 'wrap' is off and the line continues beyond the right of the screen
-listchars = listchars .. "extends:#,"
--- Place a '#' in the first column when 'wrap' is off and there is text preceding the character visible in the first
--- column
-listchars = listchars .. "precedes:#,"
--- Show non-breaking space characters as ¬
-listchars = listchars .. "nbsp:¬,"
--- Show tabs as ▸ followed by spaces
-listchars = listchars .. "tab:▸ ,"
--- Show trailing spaces as ◊
-listchars = listchars .. "trail:◊,"
-opt("w", "listchars", listchars)
+vim.opt.list = true
+vim.opt.listchars = {
+    -- Place a '#' in the last column when 'wrap' is off and the line continues beyond the right of the screen
+    extends = "#",
+    -- Place a '#' in the first column when 'wrap' is off and there is text preceding the character visible in the
+    -- first column
+    precedes = "#",
+    -- Show non-breaking space characters as ¬
+    nbsp = "¬",
+    -- Show tabs as ▸ followed by spaces
+    tab = "▸ ",
+    -- Show trailing spaces as ◊
+    trail = "◊",
+}
 
 -- When scrolling to the top or bottom of the screen, keep 2 lines between the cursor and the edge of the screen
-opt("o", "scrolloff", 2)
+vim.opt.scrolloff = 2
 
 -- Configure and enable spell checking (without capitalization check)
-opt("b", "spelllang", "en_us")
-opt("b", "spellfile", "~/.local/share/nvim/en.utf-8.add")
-opt("b", "spellcapcheck", "")
-opt("w", "spell", true)
+vim.opt.spelllang = "en_us"
+vim.opt.spellfile = "~/.local/share/nvim/en.utf-8.add"
+vim.opt.spellcapcheck = ""
+vim.opt.spell = true
 
 -- Display line numbers with relative line numbers
-opt("w", "number", true)
-opt("w", "relativenumber", true)
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Auto-toggling of relative numbers. This will disable relative numbers for panes that do not have focus, and will
 -- also disable relative numbers in insert mode. Don't mess with relative numbers if the buffer doesn't have numbers
@@ -156,14 +144,14 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter" }, {
 
 -- When opening a new line and no filetype-specific indenting is enabled, keep the same indent as the line you're
 -- currently on. Useful for READMEs, etc
-opt("b", "autoindent", true)
+vim.opt.autoindent = true
 
 -- Default indentation settings. Display tabs as four characters wide, insert tabs as 4 spaces
 local indent = 4
-opt("b", "tabstop", indent)
-opt("b", "softtabstop", indent)
-opt("b", "shiftwidth", indent)
-opt("b", "expandtab", true)
+vim.opt.tabstop = indent
+vim.opt.softtabstop = indent
+vim.opt.shiftwidth = indent
+vim.opt.expandtab = true
 
 -- Filetype-specific overrides for indentation
 local override_indents = vim.api.nvim_create_augroup("override_indents", {})
@@ -173,7 +161,7 @@ vim.api.nvim_create_autocmd(
 )
 
 -- By default, don't wrap text
-opt("w", "wrap", false)
+vim.opt.wrap = false
 
 -- Filetype-specific overrides for 'textwidth'
 local override_textwidth = vim.api.nvim_create_augroup("override_textwidth", {})
@@ -187,22 +175,22 @@ vim.api.nvim_create_autocmd(
 )
 
 -- Add a ruler to visually indicate the file's textwidth setting
-opt("w", "colorcolumn", "+1")
+vim.opt.colorcolumn = "+1"
 
 -- Set the default location of window splits
-opt("o", "splitbelow", true)
-opt("o", "splitright", true)
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 -- Enable code folding using Treesitter, initialize with folds disabled
-opt("w", "foldmethod", "expr")
-opt("w", "foldexpr", "nvim_treesitter#foldexpr()")
-opt("w", "foldlevel", 99)
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
 
 -- Speed up CursorHold autocommand events
-opt("o", "updatetime", 300)
+vim.opt.updatetime = 300
 
 -- Set pop-up-menu transparency
-opt("o", "pumblend", 30)
+vim.opt.pumblend = 30
 
 -- Disable unnecessary built-in plugins
 local disabled_built_ins = {
@@ -226,7 +214,7 @@ local disabled_built_ins = {
     "matchit",
 }
 for _, plugin in pairs(disabled_built_ins) do
-    g["loaded_" .. plugin] = 1
+    vim.g["loaded_" .. plugin] = 1
 end
 
 -- Avoid showing extra messages when using completion: append 'c'
@@ -234,15 +222,15 @@ end
 vim.o.shortmess = string.gsub(vim.o.shortmess, "F", "") .. "c"
 
 -- Highlight text after yanking it
-cmd("autocmd TextYankPost * lua vim.highlight.on_yank {on_visual = false}")
+vim.cmd("autocmd TextYankPost * lua vim.highlight.on_yank {on_visual = false}")
 
 --
 -- Colors
 --
 
-cmd("colorscheme tokyonight")
+vim.cmd("colorscheme tokyonight")
 
-opt("o", "termguicolors", true)
+vim.opt.termguicolors = true
 
 -- barbar.nvim
 -- Make all modified buffers bold
