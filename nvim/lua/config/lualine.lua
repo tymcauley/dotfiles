f01b7ea4303311391ddbd6d764675029258ef9a0
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2022-08-10 16:58:32 -0700

    nvim: Reorganize lualine config

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -26,88 +26,83 @@ local function mode_single_character()
 end
 
 M.setup = function()
-    local ll_options = {
-        section_separators = { left = "", right = "" },
-        component_separators = { left = "", right = "" },
-        theme = "tokyonight",
-        disabled_filetypes = { "packer" },
-        globalstatus = true,
-    }
-
-    local ll_sections = {
-        lualine_a = { mode_single_character },
-        lualine_b = {
-            -- Prefix file name with file icon
-            {
-                "filetype",
-                icon_only = true,
-                separator = "",
-                padding = { left = 1, right = 0 },
-            },
-            {
-                "filename",
-                path = 0,
-            },
-        },
-        lualine_c = {
-            {
-                "b:gitsigns_head",
-                icon = "",
-            },
-            {
-                "b:gitsigns_status",
-            },
-            -- Display code context
-            {
-                navic.get_location,
-                cond = navic.is_available,
-            },
-        },
-        lualine_x = {
-            -- Display nvim-metals status for Scala files
-            {
-                function()
-                    return vim.g["metals_status"] or ""
-                end,
-                cond = function()
-                    local filetype = vim.bo.filetype
-                    return filetype == "scala" or filetype == "sbt"
-                end,
-            },
-            -- LSP diagnostics
-            {
-                "diagnostics",
-                sources = { "nvim_diagnostic" },
-            },
-        },
-        lualine_y = {
-            {
-                "filetype",
-                icons_enabled = false,
-            },
-        },
-        lualine_z = {
-            -- Display file format for non-unix files
-            {
-                "fileformat",
-                cond = function()
-                    return vim.bo.fileformat ~= "unix"
-                end,
-            },
-            "progress",
-            "location",
-        },
-    }
-
-    local ll_inactive_sections = {
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-    }
-
     ll.setup({
-        options = ll_options,
-        sections = ll_sections,
-        inactive_sections = ll_inactive_sections,
+        options = {
+            section_separators = { left = "", right = "" },
+            component_separators = { left = "", right = "" },
+            theme = "tokyonight",
+            globalstatus = true,
+        },
+
+        sections = {
+            lualine_a = { mode_single_character },
+            lualine_b = {
+                -- Prefix file name with file icon
+                {
+                    "filetype",
+                    icon_only = true,
+                    separator = "",
+                    padding = { left = 1, right = 0 },
+                },
+                {
+                    "filename",
+                    path = 0,
+                },
+            },
+            lualine_c = {
+                {
+                    "b:gitsigns_head",
+                    icon = "",
+                },
+                {
+                    "b:gitsigns_status",
+                },
+                -- Display code context
+                {
+                    navic.get_location,
+                    cond = navic.is_available,
+                },
+            },
+            lualine_x = {
+                -- Display nvim-metals status for Scala files
+                {
+                    function()
+                        return vim.g["metals_status"] or ""
+                    end,
+                    cond = function()
+                        local filetype = vim.bo.filetype
+                        return filetype == "scala" or filetype == "sbt"
+                    end,
+                },
+                -- LSP diagnostics
+                {
+                    "diagnostics",
+                    sources = { "nvim_diagnostic" },
+                },
+            },
+            lualine_y = {
+                {
+                    "filetype",
+                    icons_enabled = false,
+                },
+            },
+            lualine_z = {
+                -- Display file format for non-unix files
+                {
+                    "fileformat",
+                    cond = function()
+                        return vim.bo.fileformat ~= "unix"
+                    end,
+                },
+                "progress",
+                "location",
+            },
+        },
+
+        inactive_sections = {
+            lualine_c = { "filename" },
+            lualine_x = { "location" },
+        },
     })
 end
 
