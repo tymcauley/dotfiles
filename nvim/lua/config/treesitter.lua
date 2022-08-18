5c287f0127f7eb3375a6666581e0261a7965f7c2
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2022-08-17 22:40:40 -0700

    nvim: Update macOS gcc version

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 -- Haskell treesitter plugin uses C++11 features, but macOS clang doesn't enable those by default.
 if vim.fn.has("mac") == 1 then
-    require("nvim-treesitter.install").compilers = { "gcc-11" }
+    require("nvim-treesitter.install").compilers = { "gcc-12" }
 end
 
 require("nvim-treesitter.configs").setup({
