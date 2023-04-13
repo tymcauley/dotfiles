cf670757bfc896144c370b1be83395f7751d6ed0
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-04-13 14:24:13 -0700

    terminal: Use terminal variant of Iosevka font
    This fixes some unicode characters that were being rendered too large.

--- terminal/wezterm.lua
+++ terminal/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "tokyonight_storm"
-config.font = wezterm.font("Iosevka Nerd Font")
+config.font = wezterm.font("IosevkaTerm Nerd Font") -- TODO use the "Fixed" variant, it disables ligatures
 config.front_end = "WebGpu"
 config.term = "wezterm"
 
