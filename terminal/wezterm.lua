629d422d2f534554d36ec48c843e8cc3ccdd8580
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-04-14 14:24:38 -0700

    wezterm: Enable scrollbar

--- terminal/wezterm.lua
+++ terminal/wezterm.lua
@@ -2,8 +2,20 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "tokyonight_storm"
+-- Appearance
+
+local color_scheme_name = "tokyonight_storm"
+local color_scheme = wezterm.color.get_builtin_schemes()[color_scheme_name]
+
+config.color_scheme = color_scheme_name
+config.colors = {
+    scrollbar_thumb = color_scheme["selection_fg"],
+}
+config.enable_scroll_bar = true
 config.font = wezterm.font("IosevkaTerm Nerd Font") -- TODO use the "Fixed" variant, it disables ligatures
+
+-- System
+
 config.front_end = "WebGpu"
 config.term = "wezterm"
 
