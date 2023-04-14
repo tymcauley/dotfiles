67f3f7f86c061a13a91e02eda7612e7c02b7df9d
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-04-14 14:15:26 -0700

    terminal: Remove wezterm colorscheme, it's already built in

--- terminal/install.mk
+++ terminal/install.mk
@@ -6,4 +6,3 @@ terminal:
 	./install_file.sh terminal/kitty.conf            ~/.config/kitty/kitty.conf
 	./install_file.sh terminal/wezterm.lua           ~/.config/wezterm/wezterm.lua
 	./install_file.sh terminal/tokyonight_storm.conf ~/.config/kitty/tokyonight_storm.conf
-	./install_file.sh terminal/tokyonight_storm.toml ~/.config/wezterm/colors/tokyonight_storm.toml
