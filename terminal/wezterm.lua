04ec0d498a909a3fce0268b9f4fc91017be06d0b
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-04-16 18:58:48 -0700

    wezterm: Add custom key bindings, set up unix domain

--- terminal/wezterm.lua
+++ terminal/wezterm.lua
@@ -14,6 +14,58 @@ config.colors = {
 config.enable_scroll_bar = true
 config.font = wezterm.font("IosevkaTerm Nerd Font") -- TODO use the "Fixed" variant, it disables ligatures
 
+-- Key bindings
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+    -- Tabs
+    { key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+    { key = "n", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+    { key = "p", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+    { key = "a", mods = "LEADER", action = wezterm.action.ActivateLastTab },
+
+    -- Panes
+    { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+    { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+    { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+    { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+    { key = "s", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "v", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+    -- Navigation
+    { key = "g", mods = "LEADER", action = wezterm.action.ScrollToTop },
+    { key = "G", mods = "LEADER|SHIFT", action = wezterm.action.ScrollToBottom },
+    { key = "UpArrow", mods = "SHIFT", action = wezterm.action.ScrollToPrompt(-1) },
+    { key = "DownArrow", mods = "SHIFT", action = wezterm.action.ScrollToPrompt(1) },
+
+    -- Search
+    { key = "/", mods = "LEADER", action = wezterm.action.Search({ CaseSensitiveString = "" }) },
+
+    -- Copy mode
+    { key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+
+    -- Domains
+    { key = "d", mods = "LEADER", action = wezterm.action.DetachDomain("CurrentPaneDomain") },
+
+    -- Misc
+    {
+        -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+        key = "a",
+        mods = "LEADER|CTRL",
+        action = wezterm.action.SendString("\x01"),
+    },
+}
+
+-- Domains
+
+config.unix_domains = {
+    {
+        name = "unix",
+    },
+}
+-- Connect to the unix domain on startup.
+config.default_gui_startup_args = { "connect", "unix" }
+
 -- System
 
 config.front_end = "WebGpu"
