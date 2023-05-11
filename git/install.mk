e00201c33b072f1bbf0127ae432ce8944ab2e12f
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-05-11 07:33:41 -0700

    git: Use tokyonight theme for delta
    Theme from folke/tokyonight.nvim extras/delta/tokyonight_storm.gitconfig

--- git/install.mk
+++ git/install.mk
@@ -2,5 +2,6 @@
 ALL_TARGETS += git
 
 git:
-	./install_file.sh git/gitconfig        ~/.gitconfig
-	./install_file.sh git/gitignore_global ~/.gitignore_global
+	./install_file.sh git/gitconfig                  ~/.gitconfig
+	./install_file.sh git/gitignore_global           ~/.config/git/gitignore_global
+	./install_file.sh git/tokyonight_storm.gitconfig ~/.config/git/tokyonight_storm.gitconfig
