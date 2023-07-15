a1a926cba22eb64901a3fc8f18b91896b7e23fb4
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-07-14 18:39:47 -0700

    nvim: Fix deprecated nerdfont icons

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -49,7 +49,7 @@ return {
         },
         config = function(_, opts)
             -- Customize diagnostic symbols in the gutter
-            local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+            local signs = { Error = "󰅚 ", Warn = " ", Hint = "󰌶 ", Info = " " }
             for type, icon in pairs(signs) do
                 local hl = "DiagnosticSign" .. type
                 vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
