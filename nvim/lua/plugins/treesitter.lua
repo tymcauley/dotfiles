85b2fa9cc3a50b019702b74b7d60068bf759c5d7
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-08-06 16:51:15 -0700

    nvim: Replace deprecated function calls

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return {
             -- Disable treesitter if the file is too large
             local disable_fn = function(_, buf)
                 local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
                 if ok and stats and stats.size > max_filesize then
                     return true
                 end
