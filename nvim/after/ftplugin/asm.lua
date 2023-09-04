7492847e1cf1e79af0632e9e2267ee1f74dadbf4
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-09-04 15:14:42 -0700

    nvim: Update assembly comment character

--- nvim/after/ftplugin/asm.lua
+++ nvim/after/ftplugin/asm.lua
@@ -1,6 +1,11 @@
 vim.opt_local.textwidth = 79
+
 -- Use 8-character-wide tabs
 vim.opt_local.tabstop = 8
 vim.opt_local.softtabstop = 8
 vim.opt_local.shiftwidth = 8
 vim.opt_local.expandtab = false
+
+-- Set comment character to '#'
+vim.opt_local.commentstring = "# %s"
+vim.opt_local.comments = "b:#,fb:-"
