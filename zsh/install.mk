06cfbec24c08d66167c42302fb28a8ee3a62db1f
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-09-13 12:12:47 -0700

    zsh: Use custom zshrc instead of oh-my-zsh

--- zsh/install.mk
+++ zsh/install.mk
@@ -1,19 +1,13 @@
 .PHONY: zsh
 ALL_TARGETS += zsh
 
-OMZ_DIR := ~/.oh-my-zsh
-
-ifeq ($(wildcard $(OMZ_DIR)/.),)
-$(error Please install oh-my-zsh: >>> \
-	sh -c "$$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended \
-	<<<)
-endif
+ZSH_DIR := ~/.zsh
 
 zsh:
-	mkdir -p $(OMZ_DIR)/completions
+	mkdir -p $(ZSH_DIR)/completions $(ZSH_DIR)/plugins
 	./install_file.sh zsh/p10k.zsh       ~/.p10k.zsh
 	./install_file.sh zsh/zshrc          ~/.zshrc
 	./install_file.sh zsh/git.zsh        ~/.config/git.zsh
 	./install_file.sh zsh/editor.zsh     ~/.config/editor.zsh
 	./install_file.sh zsh/fzf-config.zsh ~/.config/fzf-config.zsh
-	./git_clone_or_pull.sh https://github.com/romkatv/powerlevel10k.git $(OMZ_DIR)/custom/themes/powerlevel10k
+	./git_clone_or_pull.sh https://github.com/romkatv/powerlevel10k.git $(ZSH_DIR)/plugins/powerlevel10k
