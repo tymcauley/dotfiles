334721e8a0878cddac4132849f771d0d42bc7aa7
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-10-02 08:44:17 -0700

    nvim: Update indent-blankline config for v3

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -92,14 +92,8 @@ return {
     {
         "lukas-reineke/indent-blankline.nvim",
         event = { "BufReadPost", "BufNewFile" },
-        config = function()
-            require("indent_blankline").setup({
-                filetype_exclude = { "help", "lazy" },
-                show_current_context = true,
-                show_trailing_blankline_indent = false,
-                disable_with_nolist = true,
-            })
-        end,
+        main = "ibl",
+        opts = {},
     },
 
     -- Add highlighting blocks around nested scope levels
