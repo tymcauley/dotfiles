8ede2184adb1360ddce2d94a33b6848c2dcc8d5e
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2023-12-09 10:38:46 -0800

    wezterm: Disable unix domain
    This is causing major screen-refresh issues with vim, that make vim very
    hard to use.

--- terminal/wezterm.lua
+++ terminal/wezterm.lua
@@ -69,13 +69,14 @@ end
 
 -- Domains
 
-config.unix_domains = {
-    {
-        name = "unix",
-    },
-}
--- Connect to the unix domain on startup.
-config.default_gui_startup_args = { "connect", "unix" }
+-- Disable while this causes screen-refresh issues with vim
+-- config.unix_domains = {
+--     {
+--         name = "unix",
+--     },
+-- }
+-- -- Connect to the unix domain on startup.
+-- config.default_gui_startup_args = { "connect", "unix" }
 
 -- Don't remove the "SSH_CLIENT" variable from the multiplexer server environment when connecting over SSH, it's useful
 -- for prompts to detect if the shell is running over ssh.
