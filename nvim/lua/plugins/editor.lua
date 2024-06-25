87e3efabccba5f4bb67cc18cb639125496c51af9
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2024-06-25 16:39:24 -0700

    nvim: Update to trouble.nvim v3

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -126,36 +126,37 @@ return {
     -- Window for diagnostics
     {
         "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = { use_diagnostic_signs = true },
-        cmd = { "TroubleToggle", "Trouble" },
+        opts = {},
+        cmd = "Trouble",
         keys = {
             {
                 "<leader>xx",
-                function()
-                    require("trouble").toggle("document_diagnostics")
-                end,
-                desc = "Document Diagnostics (Trouble)",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
             },
             {
                 "<leader>xX",
-                function()
-                    require("trouble").toggle("workspace_diagnostics")
-                end,
-                desc = "Workspace Diagnostics (Trouble)",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
             },
             {
                 "<leader>xL",
-                function()
-                    require("trouble").toggle("loclist")
-                end,
+                "<cmd>Trouble loclist toggle<cr>",
                 desc = "Location List (Trouble)",
             },
             {
                 "<leader>xQ",
-                function()
-                    require("trouble").toggle("quickfix")
-                end,
+                "<cmd>Trouble qflist toggle<cr>",
                 desc = "Quickfix List (Trouble)",
             },
         },
