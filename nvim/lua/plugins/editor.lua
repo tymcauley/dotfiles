cb3ab334da95771c1a5be7f68052bee24cee3a11
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2024-12-12 10:47:48 -0800

    nvim: Fix build errors for telescope-fzf-native

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -88,7 +88,7 @@ return {
             -- Improve fuzzy finding performance for `telescope.nvim`
             {
                 "nvim-telescope/telescope-fzf-native.nvim",
-                build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+                build = "make",
             },
             -- Picker for live-grep args
             { "nvim-telescope/telescope-live-grep-args.nvim" },
