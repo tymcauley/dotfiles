7cf9f05e446c4cbd6f0f74e60f8edc58be46c1ec
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2025-01-01 19:08:42 -0800

    terminal: Add initial ghostty config

--- terminal/install.mk
+++ terminal/install.mk
@@ -3,6 +3,7 @@ ALL_TARGETS += terminal
 
 terminal:
 	./install_file.sh terminal/alacritty.yml         ~/.config/alacritty/alacritty.yml
+	./install_file.sh terminal/ghostty.config        ~/.config/ghostty/config
 	./install_file.sh terminal/kitty.conf            ~/.config/kitty/kitty.conf
 	./install_file.sh terminal/wezterm.lua           ~/.config/wezterm/wezterm.lua
 	./install_file.sh terminal/tokyonight_storm.conf ~/.config/kitty/tokyonight_storm.conf
