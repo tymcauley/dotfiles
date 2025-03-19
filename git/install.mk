5f57de4b51659b6a11bb90222add9bf471ce12c9
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2025-03-19 11:11:10 -0700

    git: Enable local commit-signature verification

--- git/install.mk
+++ git/install.mk
@@ -5,3 +5,4 @@ git:
 	./install_file.sh git/gitconfig                  ~/.gitconfig
 	./install_file.sh git/gitignore_global           ~/.config/git/gitignore_global
 	./install_file.sh git/tokyonight_storm.gitconfig ~/.config/git/tokyonight_storm.gitconfig
+	./install_file.sh git/allowed_signers            ~/.config/git/allowed_signers
