a851327245fc5bce56b08aad7b9f81da35f5ed5a
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2025-04-01 12:13:33 -0700

    nvim: Disable LSP folding for Scala

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -52,8 +52,9 @@ return {
                         return
                     end
 
-                    -- Use LSP folding if the client supports it
-                    if client:supports_method("textDocument/foldingRange") then
+                    -- Use LSP folding if the client supports it. Don't enable for Scala, since the folding support
+                    -- there just causes problems.
+                    if vim.bo.filetype ~= "scala" and client:supports_method("textDocument/foldingRange") then
                         local win = vim.api.nvim_get_current_win()
                         vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
                     end
