aebf6fe93c6fba6c5439a5d81763d7785184d823
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2026-04-24 21:57:34 -0700

    rust: Use rust-analyzer provided by rustup

--- rust/install.mk
+++ rust/install.mk
@@ -5,21 +5,7 @@ TOOL_TARGETS += rust
 rust_TOOL := rustup
 rust_HINT := https://rustup.rs
 
-ifeq ($(OS),Darwin)
-ifeq ($(ARCH),arm64)
-RUST_ANALYZER_NAME := rust-analyzer-aarch64-apple-darwin
-else
-RUST_ANALYZER_NAME := rust-analyzer-x86_64-apple-darwin
-endif
-else
-RUST_ANALYZER_NAME := rust-analyzer-x86_64-unknown-linux-gnu
-endif
-RUST_ANALYZER_URL := https://github.com/rust-lang/rust-analyzer/releases/latest/download/$(RUST_ANALYZER_NAME).gz
-
-# TODO: Install zsh completions for rustup?
-
 rust: ## Update rustup, install rust-analyzer
 	@$(call check-tool,$(rust_TOOL),$(rust_HINT))
 	rustup update
-	curl -L $(RUST_ANALYZER_URL) | gunzip -c - > ~/.local/bin/rust-analyzer
-	chmod +x ~/.local/bin/rust-analyzer
+	rustup component add rust-analyzer
