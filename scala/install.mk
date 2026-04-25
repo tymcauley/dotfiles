8dc0fdad7117d9c01a0229a38d139f8ae64d2bf7
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2026-04-24 21:47:58 -0700

    scala: Remove scalafmt/scalafix config files
    These are defined in each project, not globally.

--- scala/install.mk
+++ scala/install.mk
@@ -5,11 +5,6 @@ TOOL_TARGETS += scala
 scala_TOOL := cs
 scala_HINT := https://get-coursier.io/
 
-# TODO: Install zsh completions for coursier?
-# TODO: Install scalafix/scalafmt?
-
-scala: ## Update coursier, install scalafmt/scalafix configs
+scala: ## Update coursier
 	@$(call check-tool,$(scala_TOOL),$(scala_HINT))
 	cs update
-	./install_file.sh scala/scalafix.conf ~/.scalafix.conf
-	./install_file.sh scala/scalafmt.conf ~/.scalafmt.conf
